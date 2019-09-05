0328ed4b3dc7d9daadac4f04ac1ffcabebb50f8e
Curtis Alexander <calex@calex.org>
2019-09-05 17:39:35 +0100

    develop via REPL

---
+++ derse.fsx
@@ -0,0 +1,48 @@
+open System.IO
+
+let summarizeFiles (dir, fileSeq) =
+    let fileSize (_, size) = size
+    let fileSizeFloat(_, size) = float size
+    let fileExt (file, _) = Path.GetExtension file
+
+    let totalSize = fileSeq |> Seq.sumBy fileSize
+    let avgSize = fileSeq |> Seq.averageBy fileSizeFloat
+    let fileCnt = fileSeq |> Seq.length
+    let exs = fileSeq |> Seq.map fileExt |> Seq.distinct |> List.ofSeq
+
+    dir, totalSize, avgSize, fileCnt, exs
+
+type FileSummary =
+    {
+        Directory : string
+        TotalFileSize : int
+        AverageFileSize : float
+        FileCount : int
+        FileExtensions : string list
+    }
+
+let toFileSummary (dir, totalSize, avgSize, fileCnt, exts) =
+    {
+        Directory = dir
+        TotalFileSize = totalSize
+        AverageFileSize = avgSize
+        FileCount = fileCnt
+        FileExtensions = exts
+    }
+
+let summarizeDir (startingDirectory:string) =
+    Directory.EnumerateDirectories startingDirectory
+    |> Seq.collect (Directory.EnumerateFiles)
+    |> Seq.map (fun f -> f, (FileInfo f).Length |> int)
+    |> Seq.groupBy (fun (f, _) -> Path.GetDirectoryName f)
+    |> Seq.map summarizeFiles
+    |> Seq.sortByDescending (fun (_, totalSize, _, _, _) -> totalSize)
+    |> Seq.map toFileSummary
+
+
+let summarizeDirPrint (startingDirectory:string) =
+    dirSizes startingDirectory
+    |> Seq.iter (printfn "%A\n")
+
+// Testing
+// summarizeDirPrint "/Users/calex/code/fsharp-learning"
